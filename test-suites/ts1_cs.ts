a978be362c3df862d5f0fcb71997c62443fbbad7
montbaga <64852258+montbaga@users.noreply.github.com>
2023-09-06 12:53:36 +0300

    input_ts

---
+++ test-suites/ts1_cs.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ts1_cs</name>
+    <testSuiteGuid>fe3f5653-dafc-44dc-a266-41acb060f9b8</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Test Case 1</testCaseId>
+        <guid>2102f052-265e-41e0-9c3a-17a912891200</guid>
+    </testCaseLink>
+</TestSuiteEntity>
